f1b1d0264f00a8185fb134bb2a88abe49741337a
suvarna1405 <suvarna7404@gmail.com>
2025-10-29 00:31:15 +0530

    data initiated

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express()
 const User= require("./models/user");
+const user = require("./models/user");
 
 app.use(express.json());
+
 app.post("/signup",async(req,res)=>{
     const user =new User(req.body
         // firstName: "suvarna",
         // lastName:"s",
-        // enailId:"suvarna@gmail.com",
+        // emailId:"suvarna@gmail.com",
         // password:"1234567"
     ) ;
 
@@ -19,6 +21,30 @@ app.post("/signup",async(req,res)=>{
     }
 });
 
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.emailId;
+
+    try{
+        const user = await User.find({ emailId: userEmail });
+        if(user.length ===0){
+            res.status(404).send("User not found");
+        }else{
+            res.send(user);
+        }
+       
+    }catch(err){
+        res.status(400).send("something went wrong")
+    }
+});
+
+app.get("/feed",async(req,res)=>{
+try{
+    const user= await User.find({});
+    res.send(user);
+}catch(err){
+    res.status(400).send("something is wrong")
+}
+});
 
 connectDB().then(()  => {
     console.log("Database Connected Sucessfully");
